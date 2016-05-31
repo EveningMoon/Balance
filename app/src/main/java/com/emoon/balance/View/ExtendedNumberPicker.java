34e426a11f3391394e7881fcfa1ce1feab920fb2
zhanhui913 <zhanhui913@gmail.com>
2016-05-31 09:44:15 -0700

    Change number picker text size with edit text size

--- app/src/main/java/com/emoon/balance/View/ExtendedNumberPicker.java
+++ app/src/main/java/com/emoon/balance/View/ExtendedNumberPicker.java
@@ -2,7 +2,10 @@ package com.emoon.balance.View;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.NumberPicker;
 
 import java.lang.reflect.Field;
@@ -42,6 +45,31 @@ public class ExtendedNumberPicker extends NumberPicker {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void addView(View child){
+        super.addView(child);
+        updateView(child);
+    }
+
+    @Override
+    public void addView(View child, int index, android.view.ViewGroup.LayoutParams params) {
+        super.addView(child, index, params);
+        updateView(child);
+    }
+
+    @Override
+    public void addView(View child, android.view.ViewGroup.LayoutParams params) {
+        super.addView(child, params);
+        updateView(child);
+    }
+
+    private void updateView(View view) {
+        if(view instanceof EditText){
+            ((EditText) view).setTextSize(20);
+            ((EditText) view).setTextColor(Color.parseColor("#333333"));
         }
     }
 }
